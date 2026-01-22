ab9689cf50eae3e505706d49a37fda83db999350
David Gili <d.gili.lopez@gmail.com>
2026-01-22 17:04:50 -0300

    navbar con enlaces a rutas funcionando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -7,18 +8,33 @@ const Navbar = () => {
     <div className="barra">
       <div className="barra">
         Pizzería Mamma Mía!
-        <button className="botonesBarra espaciado">🍕 Home</button>
-        <button className="botonesBarra espaciado">
-          {token === true ? "🔒 Logout" : "🔐 Login"}
-        </button>
-        <button className="botonesBarra espaciado">
-          {token === true ? "🔓 Profile" : "🔐 Register"}
-        </button>
+        <Link to="/">
+          <button className="botonesBarra espaciado">🍕 Home</button>
+        </Link>
+        <Link to="/login">
+          <button className="botonesBarra espaciado">
+            {token ? "🔒 Logout" : "🔐 Login"}
+          </button>
+        </Link>
+        {token ? (
+          <Link to="/profile">
+            <button className="botonesBarra espaciado">🔓 Profile</button>
+          </Link>
+        ) : (
+          <Link to="/register">
+            <button className="botonesBarra espaciado">🔐 Register</button>
+          </Link>
+        )}
+        {/* <button className="botonesBarra espaciado">
+          {token ? "🔓 Profile" : "🔐 Register"}
+        </button> */}
       </div>
       <div>
-        <button className="botonTotal">
-          🛒 Total:${total.toLocaleString("es-CL")}
-        </button>
+        <Link to="/cart">
+          <button className="botonTotal">
+            🛒 Total:${total.toLocaleString("es-CL")}
+          </button>
+        </Link>
       </div>
     </div>
   );
