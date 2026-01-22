7b262969e418971870357032db27bd6bc82a89e4
David Gili <d.gili.lopez@gmail.com>
2026-01-22 15:10:50 -0300

    rutas agregadas en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -7,16 +8,23 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import Cart from "./pages/Cart";
 import Pizza from "./pages/Pizza";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <Home></Home>
-      {/* <RegisterPage></RegisterPage> */}
-      {/* <LoginPage></LoginPage> */}
-      {/* <Cart></Cart> */}
-      {/* <Pizza></Pizza> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/p001" element={<Pizza />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer></Footer>
     </>
   );
